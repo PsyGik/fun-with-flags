ee5b678ff858950efed7bbb2ed497e9024d609b1
PsyGik <dhanraj_padmashalli@condenast.com>
2021-12-12 12:49:45 +0530

    feat: wrap in provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { FeatureFlagProvider } from "../context/feature-flag.context";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <FeatureFlagProvider>
+      <Component {...pageProps} />
+    </FeatureFlagProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
